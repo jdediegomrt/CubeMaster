8eea7de4bf602d4850685da802ad2c77f34bb4c2
jdediegomrt <dediegomrt@gmail.com>
2018-04-11 16:29:26 +0200

    onboarding skip button, strings & layout

--- app/src/main/java/com/dediegomrt/cubemaster/View/OnboardingActivity.java
+++ app/src/main/java/com/dediegomrt/cubemaster/View/OnboardingActivity.java
@@ -1,13 +1,12 @@
 package com.dediegomrt.cubemaster.View;
 
-import android.os.Handler;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +14,6 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.dediegomrt.cubemaster.Config.PrefsConfig;
 import com.dediegomrt.cubemaster.Methods.PrefsMethods;
@@ -23,9 +21,10 @@ import com.dediegomrt.cubemaster.R;
 
 public class OnboardingActivity extends AppCompatActivity {
 
+    private RelativeLayout text;
     private TextView mainText;
     private TextView subText;
-    private RelativeLayout text;
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,24 +33,17 @@ public class OnboardingActivity extends AppCompatActivity {
 
         PrefsConfig.getInstance().setContext(this);
 
+        text = (RelativeLayout) findViewById(R.id.text);
         mainText = (TextView) findViewById(R.id.main_text);
         subText = (TextView) findViewById(R.id.sub_text);
-        text = (RelativeLayout) findViewById(R.id.text);
+        image = (ImageView) findViewById(R.id.image);
         final ImageView cubemasterImage = (ImageView) findViewById(R.id.cubemaster_image);
 
-        final Button button = (Button) findViewById(R.id.gotit_button);
+        final Button button = (Button) findViewById(R.id.onboarding_button);
         final ViewPager viewPager = (ViewPager) findViewById(R.id.onboarding_container);
 
         TabLayout pageIndicator = (TabLayout) findViewById(R.id.pageIndicator);
         pageIndicator.setupWithViewPager(viewPager, true); // <- magic here
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                PrefsMethods.getInstance().setOnboardingShown(true);
-                finish();
-            }
-        });
 
         viewPager.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
             @Override
@@ -61,7 +53,7 @@ public class OnboardingActivity extends AppCompatActivity {
 
             @Override
             public int getCount() {
-                return 4;
+                return 6;
             }
         });
 
@@ -71,35 +63,54 @@ public class OnboardingActivity extends AppCompatActivity {
                 final float normalizedPosition = Math.abs(Math.abs(position) - 1);
                 if(normalizedPosition<0.5){
                     text.setAlpha(1-normalizedPosition*2);
-                    button.setAlpha(1-normalizedPosition*2);
+                    image.setAlpha((float)0.5-normalizedPosition);
                 }else{
                     text.setAlpha(normalizedPosition*2-1);
-                    button.setAlpha(normalizedPosition*2-1);
+                    image.setAlpha(normalizedPosition-(float)0.5);
                 }
-                cubemasterImage.setRotation(360-position*360);
+                image.setRotation(360-position*360);
+                cubemasterImage.setRotation(position*360-360);
             }
         });
 
         viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            public void onPageScrollStateChanged(int state) {}
+            public void onPageScrollStateChanged(int state) {/*Do nothing*/}
 
             @Override
             public void onPageScrolled( int position , float positionOffset , int positionOffsetPixels ) {
                 if(positionOffset > 0.5) {
                     updateText(position + 1);
                     if(position==viewPager.getAdapter().getCount()-2){
-                        button.setVisibility(View.VISIBLE);
+                        button.setText(R.string.got_it);
+                    }
+                    if(position==0){
+                        image.setVisibility(View.VISIBLE);
                     }
                 }else{
                     updateText(position);
                     if(position==viewPager.getAdapter().getCount()-2){
-                        button.setVisibility(View.INVISIBLE);
+                        button.setText(R.string.skip);
+                    }
+                    if(position==0){
+                        image.setVisibility(View.INVISIBLE);
                     }
                 }
             }
 
             @Override
-            public void onPageSelected( int position ) {}
+            public void onPageSelected( int position ) {/*Do nothing*/}
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(viewPager.getCurrentItem()!=viewPager.getAdapter().getCount()-1){
+                    viewPager.setCurrentItem(viewPager.getAdapter().getCount()-1, true);
+                } else {
+                    PrefsMethods.getInstance().setOnboardingShown(true);
+                    finish();
+                }
+            }
         });
     }
 
@@ -107,8 +118,7 @@ public class OnboardingActivity extends AppCompatActivity {
 
         private static final String ARG_SECTION_NUMBER = "section_number";
 
-        public PlaceholderFragment() {
-        }
+        public PlaceholderFragment() {/*Do nothing*/}
 
         public static PlaceholderFragment newInstance(int sectionNumber) {
             PlaceholderFragment fragment = new PlaceholderFragment();
@@ -138,6 +148,13 @@ public class OnboardingActivity extends AppCompatActivity {
                 case 3:
                     sectionImage.setBackgroundResource(R.drawable.background_onboarding_4);
                     break;
+                case 4:
+                    sectionImage.setBackgroundResource(R.drawable.background_onboarding_5);
+                    break;
+                case 5:
+                    sectionImage.setBackgroundResource(R.drawable.background_onboarding_6);
+                    break;
+                default: break;
             }
             return rootView;
         }
@@ -152,15 +169,29 @@ public class OnboardingActivity extends AppCompatActivity {
             case 1:
                 mainText.setText(R.string.onboarding_main_text_2);
                 subText.setText(R.string.onboarding_sub_text_2);
+                image.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.mipmap.alarm, null));
                 break;
             case 2:
                 mainText.setText(R.string.onboarding_main_text_3);
                 subText.setText(R.string.onboarding_sub_text_3);
+                image.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.mipmap.assessment, null));
                 break;
             case 3:
                 mainText.setText(R.string.onboarding_main_text_4);
                 subText.setText(R.string.onboarding_sub_text_4);
+                image.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.mipmap.extension, null));
                 break;
+            case 4:
+                mainText.setText(R.string.onboarding_main_text_5);
+                subText.setText(R.string.onboarding_sub_text_5);
+                image.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.mipmap.settings, null));
+                break;
+            case 5:
+                mainText.setText(R.string.onboarding_main_text_6);
+                subText.setText(R.string.onboarding_sub_text_6);
+                image.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.mipmap.tick, null));
+                break;
+            default: break;
         }
     }
 
